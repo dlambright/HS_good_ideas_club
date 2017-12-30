30d4b0f83182c18f8faca55277c351c99055cb8d
Dustin <dalambri@ncsu.edu>
2017-12-29 20:54:56 -0500

    data scraping

---
+++ get_historical_csv.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import time
+import datetime
+from collections import namedtuple
+
+url = "https://api.coinmarketcap.com/v1/ticker/ripple/"
+
+for i in range(0,1000):
+    myResponse = requests.get(url)
+    #print myResponse.json()
+    asdf = myResponse.json()[0]
+    fdsa = float(asdf["price_usd"])
+
+    with open('values/values.csv', 'a+') as outFile:
+        outFile.write(str(datetime.datetime.now()) + ", " + str(fdsa) + "\n")
+    print datetime.datetime.now()
+    print i
+    time.sleep(300)
+
+
+
+
+
+
