df88e5523d8ca79a6fa6e1011f0d265f186b75fc
Dustin <lambright.dustin@gmail.com>
2018-01-01 11:55:51 -0500

    two days a scraper

--- get_historical_csv.py
+++ get_historical_csv.py
@@ -9,13 +9,16 @@ url = "https://api.coinmarketcap.com/v1/ticker/ripple/"
 for i in range(0,1000):
     myResponse = requests.get(url)
     #print myResponse.json()
-    asdf = myResponse.json()[0]
-    fdsa = float(asdf["price_usd"])
+    
+    if (myResponse.json) > 1:
+    
+    	asdf = myResponse.json()[0]
+    	fdsa = float(asdf["price_usd"])
 
-    with open('values/values.csv', 'a+') as outFile:
-        outFile.write(str(datetime.datetime.now()) + ", " + str(fdsa) + "\n")
-    print datetime.datetime.now()
-    print i
+    	with open('values/values.csv', 'a+') as outFile:
+            outFile.write(str(datetime.datetime.now()) + ", " + str(fdsa) + "\n")
+    	print datetime.datetime.now()
+    	print i
     time.sleep(300)
 
 
